7b030165c74ffa14cba816e1c1ec712f8f8e2423
kimyuna111 <umexvx@hanyang.ac.kr>
2025-05-01 06:20:27 +0900

    style: Finalize layout and central alignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,34 @@
-import { HashRouter as Router } from "react-router-dom";
-import "./App.css";
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { LanguageProvider } from "./context/language/LanguageContext";
+
 import MainPage from "./routes/mainPage/MainPage";
 import BasicLayout from "./layouts/BasicLayout";
 import AuthLayout from "./layouts/AuthLayout";
 import LoginPage from "./routes/authPage/LoginPage";
-import SingupPage from "./routes/authPage/SingupPage";
+import SignupPage from "./routes/authPage/SingupPage";
 import AboutPage from "./routes/aboutPage/AboutPage";
-import AdoptionPage, { action as adoptAction } from "./routes/adoptionPage/AdoptionPage";
+import AdoptionPage from "./routes/adoptionPage/AdoptionPage";
 import CareGuidePage from "./routes/careGuidePage/CareGuidePage";
 import ChatPage from "./routes/chatPage/ChatPage";
-import { LanguageProvider } from "./context/language/LanguageContext";
-
-const router = createBrowserRouter(
-  [
-    {
-      element: <BasicLayout />,
-      children: [
-        { index: true, element: <MainPage /> },
-        { path: "about", element: <AboutPage /> },
-        { path: "adopt", element: <AdoptionPage />, action: adoptAction },
-        { path: "care-guide", element: <CareGuidePage /> },
-        { path: "chat", element: <ChatPage /> },
-      ],
-    },
-    {
-      element: <AuthLayout />,
-      children: [
-        { path: "login", element: <LoginPage /> },
-        { path: "signup", element: <SingupPage /> },
-      ],
-    },
-  ],
-  {
-    basename: "/HopeTail-FE",
-  }
-);
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
-    <>
-      <LanguageProvider>
-        <RouterProvider router={router} />
-      </LanguageProvider>
-    </>
+    <LanguageProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/adopt" element={<AdoptionPage />} />
+          <Route path="/care-guide" element={<CareGuidePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router>
+    </LanguageProvider>
   );
 }
 
