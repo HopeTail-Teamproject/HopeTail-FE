7dc6ef3116784672995cd3ca0a59adf6b3c27d58
mhojune <mhojune@gmail.com>
2025-05-07 19:21:42 +0900

    Update Vite config and prepare for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -11,25 +11,30 @@ import CareGuidePage from "./routes/careGuidePage/CareGuidePage";
 import ChatPage from "./routes/chatPage/ChatPage";
 import { LanguageProvider } from "./context/language/LanguageContext";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      element: <BasicLayout />,
+      children: [
+        { index: true, element: <MainPage /> },
+        { path: "about", element: <AboutPage /> },
+        { path: "adopt", element: <AdoptionPage />, action: adoptAction },
+        { path: "care-guide", element: <CareGuidePage /> },
+        { path: "chat", element: <ChatPage /> },
+      ],
+    },
+    {
+      element: <AuthLayout />,
+      children: [
+        { path: "login", element: <LoginPage /> },
+        { path: "signup", element: <SingupPage /> },
+      ],
+    },
+  ],
   {
-    element: <BasicLayout />,
-    children: [
-      { index: true, element: <MainPage /> },
-      { path: "about", element: <AboutPage /> },
-      { path: "adopt", element: <AdoptionPage />, action: adoptAction },
-      { path: "care-guide", element: <CareGuidePage /> },
-      { path: "chat", element: <ChatPage /> },
-    ],
-  },
-  {
-    element: <AuthLayout />,
-    children: [
-      { path: "login", element: <LoginPage /> },
-      { path: "signup", element: <SingupPage /> },
-    ],
-  },
-]);
+    basename: "/HopeTail-FE",
+  }
+);
 
 function App() {
   return (
