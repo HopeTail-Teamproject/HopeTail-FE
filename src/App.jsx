447109bae01861af958dd8bccb1db0bbb2f6d673
kimyuna111 <umexvx@hanyang.ac.kr>
2025-05-08 21:15:38 +0900

    Add UI pages: Adopt, Bookmark, Community, Favorites, Files, Rehome, User

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "./App.css";
 import MainPage from "./routes/mainPage/MainPage";
 import BasicLayout from "./layouts/BasicLayout";
