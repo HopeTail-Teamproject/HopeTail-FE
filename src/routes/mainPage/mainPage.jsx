8a4d10a2b3fea2c934c9fd3e879a3f59f78bed2f
mhojune <mhojune@gmail.com>
2025-06-02 23:42:09 +0900

    Add: mainpage logo

--- src/routes/mainPage/mainPage.jsx
+++ src/routes/mainPage/mainPage.jsx
@@ -1,12 +1,21 @@
 import "./mainPage.css";
 import Logo from "/images/logo.png";
+import { useLanguage } from "../../context/language/LanguageContext";
 
 function MainPage() {
+  const { language } = useLanguage();
+  const mainText = {
+    ko: "함께 걷는 오늘, 반려견과의 첫 시작",
+    en: "Walking Together Today, First Step with Your Pet",
+  };
+
   return (
     <>
       <section className="mainPage">
         <img src={Logo} alt="logo" />
-        <div className="box"></div>
+        <div className="box">
+          <h1 className="main-title">{mainText[language] || mainText["ko"]}</h1>
+        </div>
       </section>
     </>
   );
