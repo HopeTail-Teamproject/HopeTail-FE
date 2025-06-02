18cb664071db012a5b1f44dcd659539fc8d32830
kimyuna111 <umexvx@hanyang.ac.kr>
2025-06-02 23:26:49 +0900

    Style: reduce image box height in CommunityPost page

--- src/lib/mock/communityData.js
+++ src/lib/mock/communityData.js
@@ -40,12 +40,12 @@ export const communityPosts = [
       views: 180,
     },
     images: [
-      "https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZG9nfGVufDB8fDB8fHww",
-      "https://images.unsplash.com/photo-1548199973-03cce0bbc87b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nfGVufDB8fDB8fHww",
-      "https://images.unsplash.com/photo-1543466835-00a7907e9de1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZG9nfGVufDB8fDB8fHww",
+      "https://images.unsplash.com/photo-1595781590027-7cc3affcff76?q=80&w=1962&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1590419690008-905895e8fe0d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8JUVEJTk3JTg4JUVDJThBJUE0JUVEJTgyJUE0fGVufDB8fDB8fHww",
+      "https://images.unsplash.com/photo-1522971901479-aa43436c3929?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8JUVEJTk3JTg4JUVDJThBJUE0JUVEJTgyJUE0fGVufDB8fDB8fHww",
     ],
     thumbnailUrl:
-      "https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZG9nfGVufDB8fDB8fHww",
+      "https://images.unsplash.com/photo-1522971901479-aa43436c3929?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8JUVEJTk3JTg4JUVDJThBJUE0JUVEJTgyJUE0fGVufDB8fDB8fHww",
   },
   {
     id: 3,
@@ -65,12 +65,12 @@ export const communityPosts = [
       views: 320,
     },
     images: [
-      "https://images.unsplash.com/photo-1548199973-03cce0bbc87b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nfGVufDB8fDB8fHww",
+      "https://media.istockphoto.com/id/2153996666/ko/%EC%82%AC%EC%A7%84/%EB%B9%84%EA%B8%80-%EA%B0%9C-%ED%95%9C-%EB%A7%88%EB%A6%AC%EA%B0%80-%EB%B0%94%EB%8B%A5%EC%97%90-%EC%95%89%EC%95%84-%EB%A7%88%EB%A5%B8-%EC%9D%8C%EC%8B%9D%EC%9D%B4-%EB%8B%B4%EA%B8%B4-%EA%B7%B8%EB%A6%87%EC%9D%84-%EB%B0%94%EB%9D%BC%EB%B3%B4%EA%B3%A0-%EC%9E%88%EB%8B%A4-%EB%A8%B9%EC%9D%B4%EB%A5%BC-%EA%B8%B0%EB%8B%A4%EB%A6%AC%EB%8A%94-%EC%A4%91.webp?a=1&b=1&s=612x612&w=0&k=20&c=JfLHhzzxgs16hsq9RHnOYIyuacWVoTMo0db-VtcVcjQ=",
       "https://images.unsplash.com/photo-1543466835-00a7907e9de1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZG9nfGVufDB8fDB8fHww",
-      "https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZG9nfGVufDB8fDB8fHww",
+      "https://media.istockphoto.com/id/2109761426/ko/%EC%82%AC%EC%A7%84/cocker-spaniel-and-his-owner-having-fun-at-home.webp?a=1&b=1&s=612x612&w=0&k=20&c=EnMjvp2MUqB3hVN3YqiXIcO9yq2mqQtK1B-VPHhBpfE=",
     ],
     thumbnailUrl:
-      "https://images.unsplash.com/photo-1548199973-03cce0bbc87b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nfGVufDB8fDB8fHww",
+      "https://media.istockphoto.com/id/2153996666/ko/%EC%82%AC%EC%A7%84/%EB%B9%84%EA%B8%80-%EA%B0%9C-%ED%95%9C-%EB%A7%88%EB%A6%AC%EA%B0%80-%EB%B0%94%EB%8B%A5%EC%97%90-%EC%95%89%EC%95%84-%EB%A7%88%EB%A5%B8-%EC%9D%8C%EC%8B%9D%EC%9D%B4-%EB%8B%B4%EA%B8%B4-%EA%B7%B8%EB%A6%87%EC%9D%84-%EB%B0%94%EB%9D%BC%EB%B3%B4%EA%B3%A0-%EC%9E%88%EB%8B%A4-%EB%A8%B9%EC%9D%B4%EB%A5%BC-%EA%B8%B0%EB%8B%A4%EB%A6%AC%EB%8A%94-%EC%A4%91.webp?a=1&b=1&s=612x612&w=0&k=20&c=JfLHhzzxgs16hsq9RHnOYIyuacWVoTMo0db-VtcVcjQ=",
   },
   {
     id: 4,
@@ -89,12 +89,12 @@ export const communityPosts = [
       views: 450,
     },
     images: [
-      "https://images.unsplash.com/photo-1543466835-00a7907e9de1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZG9nfGVufDB8fDB8fHww",
-      "https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZG9nfGVufDB8fDB8fHww",
-      "https://images.unsplash.com/photo-1548199973-03cce0bbc87b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nfGVufDB8fDB8fHww",
+      "https://images.unsplash.com/photo-1655275184566-ff2c4f08f784?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8JUVBJUIwJTk1JUVDJTk1JTg0JUVDJUE3JTgwJTIwJUVDJTgzJTlEJUVDJTlEJUJDfGVufDB8fDB8fHww",
+      "https://images.unsplash.com/photo-1655275184816-458617fdf563?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8JUVBJUIwJTk1JUVDJTk1JTg0JUVDJUE3JTgwJTIwJUVDJTgzJTlEJUVDJTlEJUJDfGVufDB8fDB8fHww",
+      "https://images.unsplash.com/photo-1579596380632-8c7aabb69148?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D",
     ],
     thumbnailUrl:
-      "https://images.unsplash.com/photo-1543466835-00a7907e9de1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZG9nfGVufDB8fDB8fHww",
+      "https://images.unsplash.com/photo-1655275184566-ff2c4f08f784?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8JUVBJUIwJTk1JUVDJTk1JTg0JUVDJUE3JTgwJTIwJUVDJTgzJTlEJUVDJTlEJUJDfGVufDB8fDB8fHww",
   },
   {
     id: 5,
@@ -114,12 +114,12 @@ export const communityPosts = [
       views: 520,
     },
     images: [
-      "https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZG9nfGVufDB8fDB8fHww",
-      "https://images.unsplash.com/photo-1548199973-03cce0bbc87b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nfGVufDB8fDB8fHww",
-      "https://images.unsplash.com/photo-1543466835-00a7907e9de1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZG9nfGVufDB8fDB8fHww",
+      "https://images.unsplash.com/photo-1549297161-14f79605a74c?q=80&w=1851&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://media.istockphoto.com/id/1364081718/ko/%EC%82%AC%EC%A7%84/%EC%9E%A5%EB%82%9C%EA%B0%90-%EB%8F%84%EB%84%9B%EA%B3%BC-%EA%B3%A8%EB%93%A0-%EB%91%90%EB%93%A4-%EA%B0%9C.webp?a=1&s=612x612&w=0&k=20&c=35ZSZjYwqst-K_bQQQSvKHG_6sGGAo3aAfAp9vHgouQ=",
+      "https://images.unsplash.com/photo-1597506327062-2e214954a12c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8",
     ],
     thumbnailUrl:
-      "https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZG9nfGVufDB8fDB8fHww",
+      "https://images.unsplash.com/photo-1549297161-14f79605a74c?q=80&w=1851&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 6,
@@ -138,12 +138,12 @@ export const communityPosts = [
       views: 280,
     },
     images: [
-      "https://images.unsplash.com/photo-1548199973-03cce0bbc87b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nfGVufDB8fDB8fHww",
-      "https://images.unsplash.com/photo-1543466835-00a7907e9de1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZG9nfGVufDB8fDB8fHww",
-      "https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZG9nfGVufDB8fDB8fHww",
+      "https://images.unsplash.com/photo-1597603413826-cd1c06b05222?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8JUVBJUIwJTk1JUVDJTk1JTg0JUVDJUE3JTgwJTIwJUVCJUFGJUI4JUVDJTlBJUE5fGVufDB8fDB8fHww",
+      "https://plus.unsplash.com/premium_photo-1675881733580-5d9ef42f7b03?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8JUVBJUIwJTk1JUVDJTk1JTg0JUVDJUE3JTgwJTIwJUVCJUFGJUI4JUVDJTlBJUE5fGVufDB8fDB8fHww",
+      "https://plus.unsplash.com/premium_photo-1675881733564-a9fb4e223ffe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fHw%3D",
     ],
     thumbnailUrl:
-      "https://images.unsplash.com/photo-1548199973-03cce0bbc87b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nfGVufDB8fDB8fHww",
+      "https://plus.unsplash.com/premium_photo-1675881733564-a9fb4e223ffe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fHw%3D",
   },
 ];
 
